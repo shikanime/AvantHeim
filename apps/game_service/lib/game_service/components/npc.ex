2578f9a8839cc94d5b8a85dc973939fadd33c000
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-10-07 23:24:51 +0000

    [skip ci] Add NPC

--- apps/game_service/lib/game_service/components/npc.ex
+++ apps/game_service/lib/game_service/components/npc.ex
@@ -2,4 +2,24 @@ defmodule GameService.NPCComponents do
   @moduledoc """
   TODO: Documentation for GameService.NPCComponents
   """
+
+  defmodule NpcComponent do
+    use ElvenGard.ECS.Component, state: [:vnum]
+  end
+
+  defmodule DialogComponent do
+    use ElvenGard.ECS.Component, state: [:dialog_id]
+  end
+
+  defmodule EffectComponent do
+    use ElvenGard.ECS.Component, state: [:delay]
+  end
+
+  defmodule QuestComponent do
+    use ElvenGard.ECS.Component, state: [:dialog_id]
+  end
+
+  defmodule ShopComponent do
+    use ElvenGard.ECS.Component, state: [:name, :type, :tabs]
+  end
 end
